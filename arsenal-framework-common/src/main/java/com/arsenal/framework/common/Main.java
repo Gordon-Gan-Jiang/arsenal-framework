5be4408d6713cb024f4cc41d88980302fe56f7e7
Gordon.Gan <571661991@qq.com>
2024-03-26 23:36:00 +0800

    add common module

--- arsenal-framework-common/src/main/java/com/arsenal/framework/common/Main.java
+++ arsenal-framework-common/src/main/java/com/arsenal/framework/common/Main.java
@@ -1,9 +1,8 @@
-package com.arsenal.framework.model;
+package com.arsenal.framework.common;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
